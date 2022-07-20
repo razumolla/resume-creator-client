99618eb655e32ec5ba2564d7553426a68691bbc4
PranoySarker <pranoysarker01@gmail.com>
2022-07-20 18:46:27 +0600

    add text

--- src/Pages/Home/Template.js
+++ src/Pages/Home/Template.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Template = () => {
     return (
-        <div className='text-center'>
+        <div className='text-center mt-10'>
             <h2 className='text-3xl font-semibold text-fuchsia-500'>Templates to win recruiters over</h2>
             <p>Wide selection of designs. Carefully optimised for clarity and impact.One click layouts - noformatting required.</p>
         </div>
