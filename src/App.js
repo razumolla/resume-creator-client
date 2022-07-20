b9c671734501e6a3f674d7206faf04223ed16d34
PranoySarker <pranoysarker01@gmail.com>
2022-07-21 03:21:56 +0600

    faq first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Template from './Pages/Home/Template';
+import Navbar from './Pages/Home/Navbar';
+import Banner from './Pages/Home/Banner';
+import Features from './Pages/Home/Features';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Resume creator</h1>
-      <Template></Template>
+
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/banner" element={<Banner />} />
+        <Route path="feature" element={<Features />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Banner from './Banner';
+import Faq from './Faq';
+
+
+const Home = () => {
+    return (
+        <div>
+
+            <Banner></Banner>
+            <Faq></Faq>
+
+        </div>
+    );
+};
+
+export default Home;
