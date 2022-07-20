7b25d5fb45fab3e2409c80c7125a92d9c4b37d67
afruzaakter <afruzaaktersonia1995@gmail.com>
2022-07-20 18:18:40 +0600

    at first assets and images folder create and banner section add

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import banner from '../Assets/Images/banner.jpg'
 
 const Banner = () => {
     return (
         <div>
-            <h1>This is banner section</h1>
+            <div class="hero min-h-screen bg-base-200">
+                <div class="hero-content flex-col lg:flex-row-reverse gap-4">
+                    <img src={banner} class="max-w-sm rounded-lg shadow-2xl" />
+                    <div className='w-70'>
+                        <h1 class="text-4xl font-bold">Create a Professional resume</h1>
+                        <p class="py-6">Fill in the blanks, choose a template, and download your resume instantly</p>
+                        <button class="btn btn-success">Create Resume</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
