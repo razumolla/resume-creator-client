80bbdfd5cac89493b7e2085a6b5418c9b9a06c2f
PranoySarker <pranoysarker01@gmail.com>
2022-07-20 18:11:18 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Template from './Pages/Home/Template';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Resume creator</h1>
-
+      <Template></Template>
     </div>
   );
 }
---
+++ src/Pages/Home/Template.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Template = () => {
+    return (
+        <div className='text-center'>
+            <h2 className='text-3xl font-semibold text-fuchsia-500'>Templates to win recruiters over</h2>
+            <p>Wide selection of designs. Carefully optimised for clarity and impact.One click layouts - noformatting required.</p>
+        </div>
+    );
+};
+
+export default Template;
