12b1729ce1b3395772348cf45903b3a3a002162e
rabiulhossen <rhsimon69@gmail.com>
2022-07-22 00:46:20 +0600

    footer completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Footer from './Pages/Home/Footer';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Resume creator</h1>
-
+<Footer />
     </div>
   );
 }
