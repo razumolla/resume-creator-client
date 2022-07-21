fc152c252561a96360ac80a7956b878eaab8b07c
afruzaakter <afruzaaktersonia1995@gmail.com>
2022-07-21 22:59:38 +0600

    update banner part and resposive

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -4,18 +4,20 @@ import banner from '../Assets/Images/banner.jpg'
 const Banner = () => {
     return (
         <div>
-            <div class="hero min-h-screen bg-base-200">
-                <div class="hero-content flex-col lg:flex-row-reverse gap-4">
-                    <img src={banner} class="max-w-sm rounded-lg shadow-2xl" />
-                    <div className='w-70'>
-                        <h1 class="text-4xl font-bold">Create a Professional resume</h1>
-                        <p class="py-6">Fill in the blanks, choose a template, and download your resume instantly</p>
-                        <button class="btn btn-success">Create Resume</button>
+            <div className="hero min-h-screen lg:bg-base-200 sm:bg-base-0">
+                <div className="hero-content flex-col lg:flex-row-reverse gap-4">
+                    <img src={banner} className="max-w-sm rounded-lg shadow-2xl" />
+                    <div className='m-20 '>
+                        <h1 className="text-4xl font-bold">Create a Professional
+            
+                         CV/Resume & Cover Letter Templates</h1>
+                        <p className="py-6">Fill in the blanks, choose a template, and download your resume instantly</p>
+                        <button className="btn btn-success">Create Resume</button>
                     </div>
                 </div>
             </div>
         </div>
-    );
+    )
 };
 
 export default Banner;
